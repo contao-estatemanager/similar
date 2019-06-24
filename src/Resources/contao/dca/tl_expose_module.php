1fe12241bcbd50021150800160a7020a0105626c
doishub <daniele@oveleon.de>
2019-06-24 09:42:35 +0200

    Remove real estate template from expose module dca

--- src/Resources/contao/dca/tl_expose_module.php
+++ src/Resources/contao/dca/tl_expose_module.php
@@ -14,46 +14,4 @@ if(ContaoEstateManager\Similar\AddonManager::valid()) {
     (
         'similar'  => '{title_legend},name,headline,type;{settings_legend},jumpTo,numberOfItems,perPage,hideOnEmpty;{image_legend:hide},imgSize;{template_legend:hide},customTpl,realEstateTemplate;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID'
     ));
-
-    // Add fields
-    array_insert($GLOBALS['TL_DCA']['tl_expose_module']['fields'], -1, array(
-        'realEstateTemplate' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_expose_module']['realEstateTemplate'],
-            'default'                 => 'real_estate_item_default',
-            'exclude'                 => true,
-            'inputType'               => 'select',
-            'options_callback'        => array('tl_module_estate_manager_similar', 'getRealEstateTemplates'),
-            'eval'                    => array('tl_class'=>'w50'),
-            'sql'                     => "varchar(64) NOT NULL default ''"
-        ),
-    ));
-}
-
-/**
- * Provide miscellaneous methods that are used by the data configuration array.
- *
- * @author Daniele Sciannimanica <daniele@oveleon.de>
- */
-class tl_module_estate_manager_similar extends \Backend
-{
-
-    /**
-     * Import the back end user object
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->import('BackendUser', 'User');
-    }
-
-    /**
-     * Return all real estate list templates as array
-     *
-     * @return array
-     */
-    public function getRealEstateTemplates()
-    {
-        return $this->getTemplateGroup('real_estate_item_');
-    }
 }
